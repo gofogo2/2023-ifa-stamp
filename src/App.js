c003e4ba319e4d927f0288b1b46ef17cf9ee94c3
늘 재밌는 세상, 고뽀고 <59677526+gofogo2@users.noreply.github.com>
2024-01-17 06:30:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -353,7 +353,7 @@ export default function App() {
               <CPopup isSuc={isSuc} isMainLang={isMainLang} />
             ) : (
               <div className="relative w-full" >
-                {aa}
+               <div className="fixed top-0 left-10" > aaa{aa}</div>
                 <div className=" flex items-center justify-center" >
                   <img src={`stamp2/${isMainLang ? "KR" : "EN"}_01_0${count}.png`} className="w-full sm:max-w-sm" alt="" />
                 </div>
