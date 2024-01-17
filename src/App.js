175675871db35006400fbd4761975967a2618aef
늘 재밌는 세상, 고뽀고 <59677526+gofogo2@users.noreply.github.com>
2024-01-17 16:55:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -266,6 +266,17 @@ export default function App() {
     };
   }, []);
 
+  checkData();
+  function checkData() {
+    const today = new Date().toDateString();
+    const savedDate = localStorage.getItem('dateSaved');
+    
+    if (today !== savedDate) {
+      console.log('날짜변경되어 초기화');
+        localStorage.clear();
+        localStorage.setItem('dateSaved', today);
+    }
+
   useEffect(() => {
 
     LoadingShow();
