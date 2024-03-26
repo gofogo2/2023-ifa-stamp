35fde9de94aaef3b3a4e703e7e41d7f735cfe4d6
gofogo <promoted.go@gmail.com>
2024-03-26 18:05:01 +0900

    A

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CPopup from "./components/CPopup";
 
 export default function App() {
   //스탬프 카운트
-  const maxLength = 4;
+  const maxLength = 5;
 
   //debug 활성화를 위한 클릭
   const clickCount = 3;
@@ -18,11 +18,12 @@ export default function App() {
   const [items, setItems] = useState(new Array(maxLength).fill(false));
   const [isError, setIsError] = useState(true);
   const [isLogin, setIsLogin] = useState(false);
+  const [isGuide, setIsGuide] = useState(true);
 
   const setShowPopupTrue = () => {
     fillData();
     setShowPopup(true);
-  }
+  };
 
   const setErrorFunc = () => {
     setIsError(false);
@@ -30,8 +31,16 @@ export default function App() {
 
   const setLoginTrue = () => {
     setIsLogin(true);
-    localStorage.setItem('islogin', 'true');
-    
+    localStorage.setItem("islogin", "true");
+  };
+
+  const setGuideTrue = () => {
+    setIsGuide(true);
+    localStorage.setItem("isGuide", "true");
+  };
+  const setGuideFalse = () => {
+    setIsGuide(false);
+    localStorage.setItem("isGuide", "false");
   };
 
   const setLoginFalse = () => {
@@ -53,64 +62,31 @@ export default function App() {
   };
 
   const toggleDebug = () => {
+    console.log(isDebug);
     setIsDebug(!isDebug);
   };
 
   const clearData = () => {
-    try {
-      localStorage.clear();
-      // alert("all data clear");
-      setLoginFalse();
-      window.location.href = "/";
-    } catch (e) {
-      console.error("clearData 오류:", e);
-
-      setErrorFunc();
-    }
+    localStorage.clear();
+    setLoginFalse();
+    window.location.href = "/";
   };
 
-  const fillData = () => {
-    try {
-      localStorage.setItem("1", true);
-      localStorage.setItem("2", true);
-      localStorage.setItem("3", true);
-      localStorage.setItem("4", true);
-      // localStorage.setItem("finish", false);
-      // alert("all data fill");
-      window.location.href = "/?stp=1";
-    } catch (e) {
-      console.error("fillData 오류:", e);
-      setErrorFunc();
-    }
+  const fillData = (count) => {
+    initData(count);
+    window.location.href = "/?stp=1";
   };
 
-  const threeData = () => {
-    try {
-      localStorage.setItem("1", true);
-      localStorage.setItem("2", true);
-      localStorage.setItem("3", true);
-      // localStorage.setItem("finish", false);
-      // alert("all data fill");
-      window.location.href = "/?stp=1";
-    } catch (e) {
-      console.error("fillData 오류:", e);
-      setErrorFunc();
+  const initData = (count) => {
+    for (let index = 0; index <= count; index++) {
+      localStorage.setItem(index.toString(), true);
     }
   };
 
   const changeFinish = () => {
-    try {
-      localStorage.setItem("1", true);
-      localStorage.setItem("2", true);
-      localStorage.setItem("3", true);
-      localStorage.setItem("4", true);
-      // localStorage.setItem("finish", true);
-      // alert("go to finish");
-      window.location.href = "/?stp=1";
-    } catch (e) {
-      console.error("fillData 오류:", e);
-      setErrorFunc();
-    }
+    initData(maxLength);
+    // localStorage.setItem("finish", true);
+    window.location.href = "/?stp=1";
   };
 
   useEffect(() => {
@@ -123,11 +99,12 @@ export default function App() {
         // setErrorFunc();
       }
 
-      if (current === 'reset') {
+      if (current === "reset") {
         SetIsSuc(true);
         setShowPopup(true);
         localStorage.clear();
         setLoginTrue();
+        setIsGuide(true);
         return;
       }
 
@@ -137,6 +114,7 @@ export default function App() {
         case "2":
         case "3":
         case "4":
+        case "5":
           current = "7777";
           break;
 
@@ -153,6 +131,9 @@ export default function App() {
         case "6f8b2e77":
           current = "4";
           break;
+        case "8db8d264":
+          current = "5";
+          break;
         default:
           break;
       }
@@ -161,17 +142,27 @@ export default function App() {
 
       var modifyItems = items;
 
-
-      if (localStorage.getItem('islogin') === null || localStorage.getItem('islogin') === 'false') {
+      if (
+        localStorage.getItem("islogin") === null ||
+        localStorage.getItem("islogin") === "false"
+      ) {
         setIsLogin(false);
-      }
-      else {
+      } else {
         setIsLogin(true);
+      }
+
+      if (
+        localStorage.getItem("isGuide") === null ||
+        localStorage.getItem("isGuide") === "true"
+      ) {
+        setIsGuide(true);
+      } else {
+        setIsGuide(false);
       }
 
       items.forEach((val, i) => {
         const reval = localStorage.getItem(i + 1);
-        
+
         if (reval === null || reval === false) {
           modifyItems[i] = false;
         } else {
@@ -192,22 +183,16 @@ export default function App() {
         }
       });
 
-console.log('a:'+cnt);
+      console.log("a:" + cnt);
       if (cnt >= maxLength) {
         setShowPopup(true);
       }
 
-      
-
-      if(current === null){
-        console.log('true');
+      if (current === null) {
+        console.log("true");
+      } else {
+        if (current !== "reset") window.location.href = "/";
       }
-      else
-      {
-        if(current !== 'reset')
-        window.location.href='/';
-      }
-
     } catch (e) {
       console.error("useEffect 오류:", e);
       setErrorFunc();
@@ -217,29 +202,39 @@ console.log('a:'+cnt);
   return (
     <>
       {true ? (
-        <div className="bg-[#F5F5F5] flex justify-center">
-          <div
-            className="fixed bottom-0 left-0 w-28 h-28 bg-red-200 opacity-0 z-[10000]"
-            onClick={debugClick}
-          ></div>
-          {showPopup ? <CPopup isSuc={isSuc} /> : (
-            <CStampPanal items={items} setShowPopupTrue={setShowPopupTrue} />
-          )}
-          <div className="flex items-center justify-center"></div>
-          {isDebug && (
-            <CDebugPopup
-              toggleDebug={toggleDebug}
-              clearData={clearData}
-              fillData={fillData}
-              threeData={threeData}
-              changeFinish={changeFinish}
-            />
-          )}
-          <div
-            className="fixed top-0 z-[10000] w-5 h-5 bg-[#00000000]"
-            onClick={() => toggleDebug()}
-          ></div>
-        </div>
+        isGuide ? (
+          <div className="relative flex justify-center bg-black">
+            <img className="relative" src="stamp/guide.png"></img>
+            {/* <button className="absolute w-full bg-blue-500 bottom-64 h-52 opacity-30 xs:bg-red-300 xxs:bg-amber-200 xxxs:bg-green-400 md:bg-purple-700 lg:bg-pink-400 "></button> */}
+            <button className="absolute bottom-0 w-full h-52 xxs:h-32 xs:h-52 md:h-72" onClick={()=>{setGuideFalse()}}></button>
+          </div>
+        ) : (
+          <div className="bg-[#F5F5F5] flex justify-center">
+            <div
+              className="fixed bottom-0 left-0 w-28 h-28 bg-red-200 opacity-0 z-[10000]"
+              onClick={debugClick}
+            ></div>
+            {showPopup ? (
+              <CPopup isSuc={isSuc} />
+            ) : (
+              <CStampPanal items={items} setShowPopupTrue={setShowPopupTrue} />
+            )}
+            <div className="flex items-center justify-center"></div>
+            {isDebug && (
+              <CDebugPopup
+                toggleDebug={toggleDebug}
+                clearData={clearData}
+                fillData={()=>fillData(maxLength)}
+                threeData={()=>fillData(3)}
+                changeFinish={changeFinish}
+              />
+            )}
+            <div
+              className="fixed top-0 z-[10000] w-5 h-5 bg-[#00000000]"
+              onClick={() => toggleDebug()}
+            ></div>
+          </div>
+        )
       ) : (
         <CLogin setLoginTrue={setLoginTrue} />
       )}
