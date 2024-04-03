42d7c8bcf2f50962a8cb2abdb10ab481e5a052df
gofogo <promoted.go@gmail.com>
2024-04-03 12:17:40 +0900

    Update CLogin.jsx

--- src/components/CLogin.jsx
+++ src/components/CLogin.jsx
@@ -9,17 +9,17 @@ const CLogin = () => {
     localStorage.setItem("islogin", "true");
   };
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-gray-50 ">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
-          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
         >
           World IT Show 2024
         </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"></h1>
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl "></h1>
             <form
               class="space-y-4 md:space-y-6"
               action="#"
@@ -38,7 +38,7 @@ const CLogin = () => {
               <div>
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-gray-900 "
                 >
                   Password
                 </label>
@@ -47,14 +47,14 @@ const CLogin = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                   required=""
                 />
               </div>
 
               <button
                 type="submit"
-                class="w-full text-white bg-[#9146FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-white bg-[#9146FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
               >
                 Sign in
               </button>
