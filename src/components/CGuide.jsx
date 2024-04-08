7906bcd738f2b059264d392921351dcc0348f867
gofogo <promoted.go@gmail.com>
2024-04-08 17:10:11 +0900

    Update CGuide.jsx

--- src/components/CGuide.jsx
+++ src/components/CGuide.jsx
@@ -13,6 +13,12 @@ const CGuide = () => {
     <div className="relative flex justify-center bg-black">
       <img className="relative" src="test/guide.png"></img>
       <button
+        className="absolute top-0 right-0 w-20 h-20 bg-transparent"
+        onClick={() => {
+          setGuideFalse();
+        }}
+      ></button>
+      <button
         className="absolute bottom-0 w-full h-52 xxs:h-32 xs:h-52 md:h-72"
         onClick={() => {
           setGuideFalse();
