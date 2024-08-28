4dd7d98aa8865a4a0c506e15149a55136d7d6cce
gofogo <promoted.go@gmail.com>
2024-08-28 11:00:20 +0900

    독일 구분

--- src/components/CDebugPopup.jsx
+++ src/components/CDebugPopup.jsx
@@ -21,7 +21,7 @@ const CDebugPopup = (props) => {
      <div
       className="py-5 text-center text-yellow-400 bg-gray-800 font-extralight"
     >
-      version : 0.19 - 10월 18일자 디자인 업데이트
+독일서버
     </div>
     <div
     onClick={props.clearData}
